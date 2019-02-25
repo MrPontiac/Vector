ed6792fc33d9f9ee0f63583da908ac4831a4d472
mrpontiac527 <mrpontiac527@gmail.com>
2019-02-25 11:52:06 -0500

    Added documentation

--- Vector.java
+++ Vector.java
@@ -5,7 +5,9 @@ class Vector {
 	private double angle;
 	private double i;
 	private double j;
-
+  /*
+  * A Private Constructor for the vector
+  */
 	private Vector(double a, double b, boolean type) {
 		if (type) {
 			magnitude = a;
@@ -28,23 +30,50 @@ class Vector {
 			}
 		}
 	}
-
+  
+  /*
+  * A static factory method that utilizes Magnitude and Angle to determine everything
+  * @param Magntude - The magnitude of the vector
+  * @param Angle - The angle of the vector
+  * @returns A vector Object
+  */
 	public static Vector VectorMA(double magnitude, double angle) {
 		return new Vector(magnitude, angle, true);
 	}
 
-	public static Vector VectorIJ(double i, double j) {
+	/*
+  * A static factory method that utilizes I and J to determine everything
+  * @param I - The x component of the vector
+  * @param J - The y component of the vector
+  * @returns A vector Object
+  */
+  public static Vector VectorIJ(double i, double j) {
 		return new Vector(i, j, false);
 	}
-
+  
+  /*
+  * Returns this vector multiplied by a variable
+  * @param multi - The variable to multiply by
+  * @returns A Vector Object
+  */
 	public Vector ScalarMulti(double multi) {
 		return new Vector(i * multi, j * multi, false);
 	}
-
+  
+  /*
+  * Returns this vector added with another Vector
+  * @param add - The vector to add
+  * @returns A Vector Object
+  */
 	public Vector VectorAdd(Vector add) {
 		return new Vector(add.i + i, add.j + j, false);
 	}
 
+  /*
+  * Returns this vector subtracted with another Vector
+  * @param sub - The vector to subtract
+  * @returns A Vector Object
+  */
 	public Vector VectorSub(Vector sub) {
 		return new Vector(i - sub.i, j - sub.j, false);
 	}
